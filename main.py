d267b05564a18b62fa855f6ffa3ad31a97050794
André Miranda Teles <161895196+dreteles@users.noreply.github.com>
2024-11-06 20:20:46 +0000

    Criando a classe PaidGamesbyYear da pergunta individual e melhorando as respostas.

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class FreePaidAnalysis:
 
     def __str__(self):
         free_percentage, paid_percentage = self.get_free_paid_percentage()
-        return f"Jogos gratuitos: {free_percentage:.2f}% | Jogos pagos: {paid_percentage:.2f}%"
+        return f"PERGUNTA 1) Qual o percentual de jogos gratuitos e pagos na plataforma? RESPOSTA >>> Jogos gratuitos: {free_percentage:.2f}% | Jogos pagos: {paid_percentage:.2f}%"
 
 
 #criando a classe YearAnalysis e método calculate_year_counts para calcular a quantidade de Games lançados por ano.
@@ -65,6 +65,30 @@ class YearAnalysis:
 
     def __str__(self):
         years = self.get_year_with_most_new_games()
-        return f"Anos(s) com maior número de novos jogos: {', '.join(map(str, years))}"
+        return f"PERGUNTA 2) Qual o ano com o maior número de novos jogos? Em caso de empate, retorne uma lista com os anos empatados. RESPOSTA >>> Anos(s) com maior número de novos jogos: {', '.join(map(str, years))}"
 
+
+#criando classe PaidGamesByYear que responde a pergunta individual do projeto: "Qual a média dos preços dos jogos pagos filtrados por ano de lançamento?" 
+
+
+class PaidGamesByYear:
+    def __init__(self, games, release_year=None):
+        self.games = games
+        self.release_year = release_year
+
+#Filtrando os jogos pagos do ano especificado
+  
+    def average_price(self):
+        filtered_games = [
+            game for game in self.games
+            if game.price > 0 and (self.release_year is None or game.release_year == self.release_year)
+        ]
+
+        total_price = sum(game.price for game in filtered_games)
+        total_paid_games = len(filtered_games)
+
+        return total_price / total_paid_games if total_paid_games else 0
+
+    def __str__(self):
+        return f"PERGUNTA 3) Qual a média dos preços dos jogos pagos filtrados por ano de lançamento? RESPOSTA >>> Preço médio dos jogos pagos lançados em {self.release_year if self.release_year else 'qualquer ano'}: R${self.average_price():.2f}"
 
