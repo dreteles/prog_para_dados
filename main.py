ddac20f59d632f67d20a8b5ef3f1ebc936332c0c
André Miranda Teles <161895196+dreteles@users.noreply.github.com>
2024-11-06 19:15:09 +0000

    Criando classe Game

--- main.py
+++ main.py
@@ -4,3 +4,14 @@
 f = open ("games_steam.csv")
 for line in f:
   print (line.strip().split(","))
+
+#criando classe Game e atribuindo condição para Games gratuitos
+#Games que não são gratuitos tem valor acima de 0.0
+class Game:
+  def __init__(self, name, price, release_year):
+      self.name = name
+      self.price = price
+      self.release_year = release_year
+
+  def is_free(self):
+      return self.price == 0.0
